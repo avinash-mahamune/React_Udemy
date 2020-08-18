588f1a1ad6c0b97008eb6aefc350104256673cb2
avinash <avinash.mahamune@diginnovators.com>
2020-08-18 21:41:34 +0530

    upto style

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    person: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 25},
+      {name: "jhon", age: 18}
+    ]
+  }
+
+
   render() {
+    
+  const style = {
+    backgroundColor: '#fff',
+    padding: '10px',
+    border: '1px solid black',
+    font: 'inherit'
+        }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p>This is really working..</p>
+
+          <button style = {style} onClick= { () => this.switchNameHandler("MAHAMUNE") }>Switch Name</button>
+          <Person name = {this.state.person[0].name} age = {this.state.person[0].age}></Person>
+          <Person name = {this.state.person[1].name} age = {this.state.person[1].age} click = {this.switchNameHandler.bind(this,"Ganesh")}></Person>
+          <Person name = {this.state.person[2].name} age = {this.state.person[2].age} changed = {this.changeNameHandler}></Person>             
+
       </div>
     );
   }
+
+    changeNameHandler = (event) => {
+      this.setState ({
+        person: [
+          {name: 'MAX', age: 28},
+          {name: "Manu", age: 25},
+          {name: event.target.value, age: 18}
+        ]
+        })
+    }
+
+  switchNameHandler = (newName) => {
+    console.log('Im Clicked');
+    this.setState ({
+      person: [
+        {name: newName, age: 28},
+        {name: "Manu", age: 25},
+        {name: "Robbin", age: 18}
+      ]
+      }
+    )
+  }
+
+ 
+
 }
 
 export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Person.css';
+
+
+const person = (props) => {
+    return (
+        <div className = "Person">
+            <p onClick = {props.click}>I'm a {props.name} and Age: {props.age}</p>
+            <p>{props.children}</p>
+            <input onChange = {props.changed} value = {props.name} />
+        </div>
+    )
+};
+
+
+export default person; 
