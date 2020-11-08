25583043e1ab23574d42a56cd1d313f90fea44ca
Avinash Mahamune <avinash.mahamune.322@gmail.com>
2020-11-08 22:07:26 +0530

    udemy

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,38 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
 
   state = {
     person: [
-      {name: "Max", age: 28},
-      {name: "Manu", age: 25},
-      {name: "jhon", age: 18}
-    ]
+      {id:'1', name: "Max", age: 28},
+      {id:'2', name: "Manu", age: 25},
+      {id:'3', name: "jhon", age: 18}
+    ],
+    showPerson: false
   }
 
 
   render() {
     
-  const style = {
-    backgroundColor: '#fff',
+  const btnStyle = {
+    backgroundColor: 'green',
+    color: '#fff',
     padding: '10px',
     border: '1px solid black',
-    font: 'inherit'
+    font: 'inherit',
+    margin: '5px 20px',
+    ':hover':{
+      backgroundColor: 'lightgreen',
+      color:'black'
+    }
         }
+   
+    let personData = null;
+
+    if(this.state.showPerson){
+      personData = (
+        <div>
+         { this.state.person.map( (item, index) =>{
+           return  <Person 
+           click= {this.deletePersonHandler.bind(index)}
+           name= {item.name} 
+           age= {item.age} 
+           key= {item.id}
+           />
+         })
+
+         } 
+      </div>
+      );
+
+      btnStyle.backgroundColor = 'red';
+      btnStyle[':hover'] = {
+        backgroundColor: 'lightcoral',
+        color:'black'
+      }
+    }
+
+    let classes = [];
+    if(this.state.person.length <=2){
+      classes.push('red');
+    }
+    if(this.state.person.length <=1){
+      // classes.pop();
+      classes.push('bold');
+    }
 
     return (
       <div className="App">
         
           <h1 className="App-title">Welcome to React</h1>
-          <p>This is really working..</p>
-
-          <button style = {style} onClick= { () => this.switchNameHandler("MAHAMUNE") }>Switch Name</button>
-          <Person name = {this.state.person[0].name} age = {this.state.person[0].age}></Person>
-          <Person name = {this.state.person[1].name} age = {this.state.person[1].age} click = {this.switchNameHandler.bind(this,"Ganesh")}></Person>
-          <Person name = {this.state.person[2].name} age = {this.state.person[2].age} changed = {this.changeNameHandler}></Person>             
-
+          <p className={classes.join(' ')}>This is really working..</p>
+          <button style = {btnStyle} onClick= { this.togglePersonHandler}>show/hide</button>
+          <button  onClick= { () => this.switchNameHandler("MAHAMUNE") }>Switch Name</button>       
+          {personData}
       </div>
     );
   }
@@ -59,8 +98,19 @@ class App extends Component {
     )
   }
 
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow});
+  }
  
+  deletePersonHandler = (personIndex) => {
+    const latestPerson = [...this.state.person];
+    latestPerson.splice(personIndex, 1);
+    this.setState({
+      person: latestPerson
+    })
+  }
 
 }
 
-export default App;
+export default Radium(App);
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -6,7 +6,7 @@ import './Person.css';
 const person = (props) => {
     return (
         <div className = "Person">
-            <p onClick = {props.click}>I'm a {props.name} and Age: {props.age}</p>
+            <p className ="info" onClick = {props.click}>I'm a {props.name} and Age: {props.age}</p>
             <p>{props.children}</p>
             <input onChange = {props.changed} value = {props.name} />
         </div>
