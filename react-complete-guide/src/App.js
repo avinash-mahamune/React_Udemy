10a9fa7c0eae8c4a6e38665fb7ce823e9e20be2a
Avinash Mahamune <avinash.mahamune.322@gmail.com>
2020-11-14 15:15:45 +0530

    asd

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Radium from 'radium';
-=======
-
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
-
->>>>>>> 29ea6042c34ee10ca5e911f33cf31be66a3dd5ae
 import Person from './Person/Person';
 import OutputUser from './Person/OutputUser';
 import UserInput from './UserInput/UserInput';
@@ -17,18 +11,10 @@ class App extends Component {
 
   state = {
     person: [
-<<<<<<< HEAD
       {id:'1', name: "Max", age: 28},
       {id:'2', name: "Manu", age: 25},
       {id:'3', name: "jhon", age: 18}
     ],
-=======
-      {name: "Max", age: 28},
-      {name: "Manu", age: 25},
-      {name: "jhon", age: 18}
-    ],
-    username: "Super Avinash",
->>>>>>> 29ea6042c34ee10ca5e911f33cf31be66a3dd5ae
     showPerson: false
   }
 
@@ -98,37 +84,10 @@ class App extends Component {
       <div className="App">
         
           <h1 className="App-title">Welcome to React</h1>
-<<<<<<< HEAD
           <p className={classes.join(' ')}>This is really working..</p>
           <button style = {btnStyle} onClick= { this.togglePersonHandler}>show/hide</button>
           <button  onClick= { () => this.switchNameHandler("MAHAMUNE") }>Switch Name</button>       
           {personData}
-=======
-          <p>This is really working..</p>
-
-          <button style = {style} onClick= { () => this.switchNameHandler("MAHAMUNE") }>Switch Name</button>
-          <button style = {style} onClick= {this.showPersonHandler}>Show Person</button>
-         
-          
-
-      {/* <OutputUser /> */}
-
-      <br /><br />
-      
-      {persons}
-
-      <h2>Assignment 1st</h2>
-      <UserInput changed = {this.changeUserNameHandler} currentName = {this.state.username} />
-      <UserOutput userName = {this.state.username} />
-      <UserOutput userName = {this.state.username} />
-      <UserOutput userName = "AVIANSH" />
-
-      <br /><br />
-      <PersonForm />
-      
-
-      <br /><br /><br /><br />
->>>>>>> 29ea6042c34ee10ca5e911f33cf31be66a3dd5ae
       </div>
     );
   }
@@ -164,7 +123,6 @@ class App extends Component {
     )
   }
 
-<<<<<<< HEAD
   togglePersonHandler = () => {
     const doesShow = this.state.showPerson;
     this.setState({showPerson: !doesShow});
@@ -177,15 +135,6 @@ class App extends Component {
       person: latestPerson
     })
   }
-=======
-  
-  changeUserNameHandler = (event)=> {
-    this.setState({
-      username : event.target.value
-    })
-  }
-
->>>>>>> 29ea6042c34ee10ca5e911f33cf31be66a3dd5ae
 
 }
 
